26664436643c0ccb252bd2ed72e78c2ea9a63d62
Shrimadhav U K <ShriMADhavUK@gmail.com>
2019-03-15 16:49:22 +0000

    FIX adminlist if creator is weird

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -193,7 +193,7 @@ def adminlist(bot: Bot, update: Update):
     text = "Admins in *{}*:".format(update.effective_chat.title or "this chat")
     for admin in administrators:
         user = admin.user
-        name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+        name = "[{}](tg://user?id={})".format((user.first_name or "Deleted Account") + (user.last_name or ""), user.id)
         if user.username:
             name = escape_markdown("@" + user.username)
         text += "\n - {}".format(name)
