KL35Ronaldo/PSonOfLars_BHMarie

commit 194e7194fedf062f138af590fde691574dad0e9a

ftgicp

tg_bot/modules/helper_funcs/string_handling.py
@@ -250,6 +250,8 @@ def extract_time(message, time_val):
         elif unit == 'h':
             bantime = int(time.time() + int(time_num) * 60 * 60)
         elif unit == 'd':
+            if time_num > 365:
+                time_num = 365
             bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
         else:
             # how even...?

commit 7c87a427e6bcaf28bb5e5769965164a7377f8335

fix derp.
regression was introduced in 194e7194fedf062f138af590fde691574dad0e9a

tg_bot/modules/helper_funcs/string_handling.py
@@ -250,7 +250,7 @@ def extract_time(message, time_val):
         elif unit == 'h':
             bantime = int(time.time() + int(time_num) * 60 * 60)
         elif unit == 'd':
-            if time_num > 365:
+            if int(time_num) > 365:
                 time_num = 365
             bantime = int(time.time() + int(time_num) * 24 * 60 * 60)
         else: